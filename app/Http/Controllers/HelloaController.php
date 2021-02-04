104057e6a0b271c1cd6fd5f61ebd2bd3233b6799
izu <calta229@gmail.com>
2021-02-04 10:58:15 +0900

    auth修正

--- app/Http/Controllers/HelloaController.php
+++ app/Http/Controllers/HelloaController.php
@@ -8,14 +8,18 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use Auth;
 
 class HelloaController extends Controller
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function indexa(){
+        if(Auth::check()){
 
             $data = DB::select('select * from products');
             return view('helloa', ['data' => $data]);
-    
+        }else{
+            return view('auth/login');
+        }
     }
 }
