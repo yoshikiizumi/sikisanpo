9e64d0954be0b3b054e3de9bee53abb9105474e1
izu <calta229@gmail.com>
2021-02-04 10:50:46 +0900

    修正２

--- app/Http/Controllers/HelloaController.php
+++ app/Http/Controllers/HelloaController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class HelloaController extends Controller
 {
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     public function indexa(){
 
             $data = DB::select('select * from products');
